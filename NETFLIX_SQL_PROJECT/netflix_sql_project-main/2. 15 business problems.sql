8539f8b8d33201db55f2610c7d68369a3c02ed02
Nenavath_Vaishnavi <pavani7013940872@gmail.com>
2024-10-12 03:39:07 +0530

    Add files via upload

---
+++ NETFLIX_SQL_PROJECT/netflix_sql_project-main/2. 15 business problems.sql
@@ -0,0 +1,42 @@
+-- Netflix Data Analysis using SQL
+-- 15 BUSINESS QUESTIONS
+
+Question 1. Count the number of Movies vs TV Shows
+
+Question 2. Find the most common rating for movies and TV shows
+
+Question 3. List all movies released in a specific year (e.g., 2020)
+
+Question 4. Find the top 5 countries with the most content on Netflix
+
+Question 5. Identify the longest movie
+
+Question 6. Find content added in the last 5 years
+
+Question 7. Find all the movies/TV shows by director 'Rajiv Chilaka'!
+
+Question 8. List all TV shows with more than 5 seasons
+
+Question 9. Count the number of content items in each genre
+
+Question 10. Find each year and the average numbers of content release by India on netflix. 
+ return top 5 year with highest avg content release !
+
+Question 11. List all movies that are documentaries
+
+Question 12. Find all content without a director
+
+Question 13. Find how many movies actor 'Salman Khan' appeared in last 10 years!
+
+Question 14. Find the top 10 actors who have appeared in the highest number of movies produced in India.
+
+Question 15:
+Categorize the content based on the presence of the keywords 'kill' and 'violence' in 
+the description field. Label content containing these keywords as 'Bad' and all other 
+content as 'Good'. Count how many items fall into each category.
+
+
+
+
+-- End of reports
+
